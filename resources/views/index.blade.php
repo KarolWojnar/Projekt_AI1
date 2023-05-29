6329b2c7df45d624edf2a01c4ea4450e7f02c41f
KarolWojnar <Wojnar@12345>
2023-05-29 19:59:05 +0200

    firstView

---
+++ resources/views/index.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Prjekt_AI1</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+  </head>
+  <body class="bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark m-2">
+        <a class="navbar-brand" href="#">Cinema Blu-ray</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Strona główna</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">O nas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Regulamin</a>
+            </li>
+          </ul>
+        </div>
+        <button class="btn btn-outline-success float-right" type="submit">Logowanie</button>
+      </nav>
+
+
+      <div id="carouselExampleInterval" class="carousel slide p-1" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-bs-interval="10000">
+            <img src="img/background3.jpg" class="d-block w-100 rounded" alt="...">
+          </div>
+          <div class="carousel-item" data-bs-interval="2000">
+            <img src="img/background1.jpg" class="d-block w-100 rounded" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/background2.jpg" class="d-block w-100 rounded" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
