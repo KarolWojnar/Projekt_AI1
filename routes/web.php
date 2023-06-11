3055acecd0713c5563acf48fbf2ac817de8a9567
KarolWojnar <Wojnar@12345>
2023-06-12 00:33:52 +0200

    Revert "asd"
    This reverts commit 49ee1b56eeaef8df20e0231fbef99f44b11e3ca3.

--- routes/web.php
+++ routes/web.php
@@ -10,37 +10,33 @@ use App\Http\Controllers\UsersController;
 use App\Http\Controllers\MoviesController;
 use App\Http\Controllers\RegulaminController;
 Auth::routes();
-
-//strona główna
-Route::get('/home', function () {return view('home');})->name('home');
-
 Route::group(['middleware' => ['web']], function () {
     Route::get('/home', function () {return view('home');});
-Route::get('/', function () {return view('home');});
-//logowanie
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
-Route::get('/logout', [LoginController::class, 'logout']);
-//rejestracja
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [RegisterController::class, 'register']);
-//regulamin
-Route::get('/regulamin', [RegulaminController::class, 'index'])->name('regulamin');
-//edycja danych
-Route::get('users/{id}/edit', [UsersController::class, 'edit'])->name('users.edit');
-Route::put('users/{id}', [UsersController::class, 'update'])->name('users.update');
-Route::put('users/{id}/loan', [UsersController::class, 'update2'])->name('users.update2');
-Route::get('/users/{id}', [UsersController::class, 'show'])->name('users.show');
-Route::get('/movie/{id}', [MoviesController::class, 'show'])->name('movies.show');
-Route::get('/movies', [MoviesController::class, 'index'])->name('movies.index');
-Route::get('/movies/filter', [MoviesController::class, 'filter'])->name('movies.filter');
-Route::get('/movie/{id}/loan', [LoansController::class, 'rentMovie'])->name('loans.show')->middleware('auth');
-Route::post('/calculate-price', [LoansController::class, 'calculatePrice'])->name('calculatePrice');
-// ADMIN
-Route::get('/editUsersAdmin',  'App\Http\Controllers\AdminPanel\EditUsersController@index')->middleware('\App\Http\Middleware\AdminMiddleware::class')->name('editUsers');
-Route::get('/editMoviesAdmin',  'App\Http\Controllers\AdminPanel\EditMoviesController@index')->middleware('\App\Http\Middleware\AdminMiddleware::class')->name('editMovies');
-Route::get('/movies/delete/{id}', [MoviesController::class, 'delete'])->name('movies.delete');
-Route::get('/users/delete/{id}', [UsersController::class, 'delete'])->name('users.delete');
-Route::put('/movies/{id}', [MoviesController::class, 'update'])->name('movies.update');
-Route::post('/movies', [MoviesController::class, 'store'])->name('movies.store');
+    Route::get('/', function () {return view('home');});
+    //logowanie
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::get('/logout', [LoginController::class, 'logout']);
+    //rejestracja
+    Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
+    //regulamin
+    Route::get('/regulamin', [RegulaminController::class, 'index'])->name('regulamin');
+    //edycja danych
+    Route::get('users/{id}/edit', [UsersController::class, 'edit'])->name('users.edit');
+    Route::put('users/{id}', [UsersController::class, 'update'])->name('users.update');
+    Route::put('users/{id}/loan', [UsersController::class, 'update2'])->name('users.update2');
+    Route::get('/users/{id}', [UsersController::class, 'show'])->name('users.show');
+    Route::get('/movie/{id}', [MoviesController::class, 'show'])->name('movies.show');
+    Route::get('/movies', [MoviesController::class, 'index'])->name('movies.index');
+    Route::get('/movies/filter', [MoviesController::class, 'filter'])->name('movies.filter');
+    Route::get('/movie/{id}/loan', [LoansController::class, 'rentMovie'])->name('loans.show')->middleware('auth');
+    Route::post('/calculate-price', [LoansController::class, 'calculatePrice'])->name('calculatePrice');
+    // ADMIN
+    Route::get('/editUsersAdmin',  'App\Http\Controllers\AdminPanel\EditUsersController@index')->middleware('\App\Http\Middleware\AdminMiddleware::class')->name('editUsers');
+    Route::get('/editMoviesAdmin',  'App\Http\Controllers\AdminPanel\EditMoviesController@index')->middleware('\App\Http\Middleware\AdminMiddleware::class')->name('editMovies');
+    Route::get('/movies/delete/{id}', [MoviesController::class, 'delete'])->name('movies.delete');
+    Route::get('/users/delete/{id}', [UsersController::class, 'delete'])->name('users.delete');
+    Route::put('/movies/{id}', [MoviesController::class, 'update'])->name('movies.update');
+    Route::post('/movies', [MoviesController::class, 'store'])->name('movies.store');
 });
